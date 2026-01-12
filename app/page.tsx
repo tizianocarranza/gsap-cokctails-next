70812bd5397db4785b960e02065b131126457be4
Tiziano Carranza <tzndevv@gmail.com>
2026-01-12 08:38:15 -0300

    Implement first part of Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+"use client"
+
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
-import Navbar from "./components/Navbar";
+import { Navbar, Hero } from "./components";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -8,6 +10,10 @@ export default function Home() {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <div className="h-dvh">
+
+      </div>
     </main>
   );
 }
