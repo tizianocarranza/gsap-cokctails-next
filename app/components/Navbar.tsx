59f45bd5cdb77b06def9c6f895dfd598dcbf8a0a
Tiziano Carranza <tzndevv@gmail.com>
2026-01-12 08:43:46 -0300

    Implement first part of Hero section

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -9,10 +9,9 @@ useGSAP(() => {
     
     navTween.fromTo("nav", 
       { backgroundColor: "transparent" }, 
-      { backgroundColor: "#00000050", backgroundFilter: "blur(10px)", duration: 1, ease: "power1.inOut" }
+      { backgroundColor: "#00000050", backdropFilter: "blur(10px)", duration: 1, ease: "power1.inOut" }
     );
 });
-
   return (
     <nav>
       <div>
